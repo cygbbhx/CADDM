4e580fcc5142e3784bf443e938f86178496cb65f
cygbbhx <cygbbhx@yonsei.ac.kr>
2024-04-12 03:30:29 +0000

    Feat: Multi-GPU setup

--- train.py
+++ train.py
@@ -13,12 +13,18 @@ import model
 from detection_layers.modules import MultiBoxLoss
 from dataset import DeepfakeDataset
 from lib.util import load_config, update_learning_rate, my_collate
-
+import os 
+from torch.utils.data.distributed import DistributedSampler
+import torch.multiprocessing as mp
 
 def args_func():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, help='The path to the config.', default='./configs/caddm_train.cfg')
     parser.add_argument('--ckpt', type=str, help='The checkpoint of the pretrained model.', default=None)
+    parser.add_argument('--local-rank', type=int, help='The local rank of GPU', default=None)
+    parser.add_argument('--sbi', type=int, help='Experimental settings for SBI. \
+                                                1) Swap-After-SBI \
+                                                2) SBI-After-Swap', default=0)
     args = parser.parse_args()
     return args
 
@@ -54,8 +60,9 @@ def load_checkpoint(ckpt, net, opt, device):
     checkpoint = torch.load(ckpt)
 
     gpu_state_dict = OrderedDict()
+    # TODO: FIX module naming convention
     for k, v in checkpoint['network'] .items():
-        name = "module."+k  # add `module.` prefix
+        name = k  # add `module.` prefix
         gpu_state_dict[name] = v.to(device)
     net.load_state_dict(gpu_state_dict)
     opt.load_state_dict(checkpoint['opt_state'])
@@ -66,14 +73,27 @@ def load_checkpoint(ckpt, net, opt, device):
 def train():
     args = args_func()
 
+    dist_url = 'env://'
+    rank = int(os.environ['RANK'])
+    world_size = int(os.environ['WORLD_SIZE'])
+    local_rank = int(os.environ['LOCAL_RANK'])
+
+    torch.distributed.init_process_group(backend='nccl', init_method=dist_url, world_size=world_size, rank=rank)
+    torch.cuda.set_device(local_rank)
+    torch.distributed.barrier()
+    if torch.distributed.get_rank() == 0:
+        print(f'RANK {rank} WORLD SIZE {world_size} LOCAL_RANK {local_rank}')
+
     # load conifigs
     cfg = load_config(args.cfg)
 
     # init model.
     net = model.get(backbone=cfg['model']['backbone'])
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    net = net.to(device)
-    net = nn.DataParallel(net)
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = local_rank
+    torch.cuda.set_device(local_rank)
+    net = net.cuda(local_rank)
+    # net = nn.DataParallel(net)
 
     # loss init
     det_criterion = MultiBoxLoss(
@@ -95,20 +115,30 @@ def train():
     # load checkpoint if given
     base_epoch = 0
     if args.ckpt:
-        net, optimzer, base_epoch = load_checkpoint(args.ckpt, net, optimizer, device)
+        net, optimzer, base_epoch = load_checkpoint(args.ckpt, net, optimizer, local_rank)
 
     # get training data
     print(f"Load deepfake dataset from {cfg['dataset']['img_path']}..")
+    cfg['sbi_key'] = args.sbi
     train_dataset = DeepfakeDataset('train', cfg)
+    sampler = DistributedSampler(dataset=train_dataset, shuffle=True)
     train_loader = DataLoader(train_dataset,
-                              batch_size=cfg['train']['batch_size'],
-                              shuffle=True, num_workers=4,
-                              collate_fn=my_collate
+                              batch_size=cfg['train']['batch_size'] // 4,
+                              num_workers=4,
+                              collate_fn=my_collate,
+                              pin_memory=True,
+                              sampler=sampler
                               )
-
+    net = torch.nn.SyncBatchNorm.convert_sync_batchnorm(net)
+    local_rank = int(os.environ['LOCAL_RANK'])
+    net = torch.nn.parallel.DistributedDataParallel(net, 
+                                                    device_ids=[local_rank],
+                                                    find_unused_parameters=True)
     # start trining.
     net.train()
     for epoch in range(base_epoch, cfg['train']['epoch_num']):
+        train_loader.sampler.set_epoch(epoch)
+        mini_batch = 0
         for index, (batch_data, batch_labels) in enumerate(train_loader):
 
             lr = update_learning_rate(epoch)
@@ -142,12 +172,18 @@ def train():
                 "lr:{:.4g}".format(lr),
             ]
             print(" ".join(outputs))
-        save_checkpoint(net, optimizer,
-                        cfg['model']['save_path'],
-                        epoch)
+            mini_batch += 1
+
+            if mini_batch == 128:
+                break
+        if torch.distributed.get_rank() == 0 and epoch % 10 == 0:
+            save_checkpoint(net, optimizer,
+                            cfg['model']['save_path'],
+                            epoch)
 
 
 if __name__ == "__main__":
+    torch.backends.cudnn.enabled = False
     train()
 
 # vim: ts=4 sw=4 sts=4 expandtab
